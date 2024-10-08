af8fa5ea60ea7185cda04c8af28a55a0db42fe75
Davidhaba <154765655+Davidhaba@users.noreply.github.com>
2024-10-08 16:40:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -144,4 +144,4 @@ def upload():
     return jsonify({'success': False})
 
 if __name__ == '__main__':
-    app.run(app, host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
